3b9008d55d2ed5e37b2395ab7cc03111b2bfa403
theofremiot <theofremiot@gmail.com>
2022-01-05 16:42:04 +0100

    detection du flux des deux caméras et détection des chessboards

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2 as cv
 
 
-cap0 = cv.VideoCapture(1)
+cap0 = cv.VideoCapture(0)
 cap1 = cv.VideoCapture(2)
 ret0, frame0 = cap0.read()
 
@@ -28,13 +28,21 @@ while True:
         print("Can't receive frame 0 (stream end?). Exiting ...")
         break
     # Our operations on the frame come here
-    gray0 = cv.cvtColor(frame0, cv.COLOR_BGR2GRAY)
-    gray1 = cv.cvtColor(frame1, cv.COLOR_BGR2GRAY)
-    dst = cv.hconcat([gray0,gray1]) 
+    # gray0 = cv.cvtColor(frame0, cv.COLOR_BGR2GRAY)
+    # gray1 = cv.cvtColor(frame1, cv.COLOR_BGR2GRAY)
+
+    retval1, corners1 = cv.findChessboardCorners(frame1, (8, 6), None)
+    retval0, corners0 = cv.findChessboardCorners(frame0, (8, 6), None)
+
+    cv.drawChessboardCorners(frame1, (8, 6), corners1, retval1)
+    cv.drawChessboardCorners(frame0, (8, 6), corners0, retval0)
+
+    dst = cv.hconcat([frame0, frame1])
     # Display the resulting frame
     cv.imshow('frame', dst)
     if cv.waitKey(1) == ord('q'):
         break
+
 # When everything done, release the capture
 cap0.release()
 cap1.release()
